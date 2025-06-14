4094a05030423eabe61012651fbea3bf27b89439
Aoi Saki <rayhansusanto36@gmail.com>
2025-06-14 22:16:18 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ export const metadata: Metadata = {
   title: "Aoi Saki",
   description: "話そうか",
   icons: {
-    icon: '/logo.png', // Path ke ikon Anda di folder public
+    icon: '/logo.ico', // Path ke ikon Anda di folder public
   },
 };
 
