01e4c628cb0c4fefcc6c6e2138415e2b4cf936fd
Aoi Saki <rayhansusanto36@gmail.com>
2025-06-18 00:15:35 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,20 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Aoi Saki",
   description: "話そう、私のプロジェクトとポートフォリオを見てください。",
+  keywords: ["Aoi Saki", "portfolio", "web developer", "video editor", "digital artist", "programmer", "3d modeler"],
+  
+  // Nama Anda sebagai penulis situs
+  authors: [{ name: "Aoi Saki" }],
+  
+  // URL utama situs Anda
+  metadataBase: new URL('https://aoisaki.cloud'), // Ganti dengan domain Anda nanti
+  
+  // Gambar yang akan muncul saat link dibagikan di media sosial
+  openGraph: {
+    title: "Aoi Saki - Portfolio",
+    description: "話そう、私のプロジェクトとポートフォリオを見てください。",
+    images: '/saki-aoi.png', // Pastikan gambar ini ada di folder /public
+  },
   icons: {
     icon: '/logo.ico', 
   },
