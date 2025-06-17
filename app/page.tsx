b94dd0fbd79bda735c9dc3057d25eaf6294d3e43
Aoi Saki <rayhansusanto36@gmail.com>
2025-06-17 20:45:47 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,23 @@
-// app/page.tsx
-
-// Menandakan komponen ini dan turunannya adalah Client Components
 'use client';
 
 // Impor komponen-komponen Anda
 import ProjectsSection from './components/project/ProjectsSection';
 import HeroSection from './components/hero/HeroSection';
 import AboutSection from "./components/about/AboutSection";
+import DrawingCanvas from './components/DrawingCanvas'; // Impor komponen kanvas
 
-// --- HALAMAN UTAMA ---
-// PERBAIKAN: Hapus 'async' dari deklarasi fungsi
 export default function HomePage() {
   return (
-    <main>
-      <HeroSection />
-      <ProjectsSection />
-      <AboutSection />
-    </main>
+    // Bungkus semua konten dalam div dengan posisi relatif
+    <div className="relative">
+      <main>
+        <HeroSection />
+        <ProjectsSection />
+        <AboutSection />
+      </main>
+      
+      {/* Tambahkan kanvas gambar di atas semua konten */}
+      <DrawingCanvas />
+    </div>
   );
 }
