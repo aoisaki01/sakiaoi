4e7bcaaee22b3942ce0d6b7c72f290789f03c8dd
Aoi Saki <rayhansusanto36@gmail.com>
2025-06-17 23:45:12 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,12 @@
 'use client';
 
-// Impor komponen-komponen Anda
 import ProjectsSection from './components/project/ProjectsSection';
 import HeroSection from './components/hero/HeroSection';
 import AboutSection from "./components/about/AboutSection";
-import DrawingCanvas from './components/DrawingCanvas'; // Impor komponen kanvas
+
 
 export default function HomePage() {
   return (
-    // Bungkus semua konten dalam div dengan posisi relatif
     <div className="relative">
       <main>
         <HeroSection />
@@ -16,8 +14,8 @@ export default function HomePage() {
         <AboutSection />
       </main>
       
-      {/* Tambahkan kanvas gambar di atas semua konten */}
-      <DrawingCanvas />
+ 
+     
     </div>
   );
 }
